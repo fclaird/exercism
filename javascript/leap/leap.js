a48399c636fb1b290b8eaad004f276d76c1cbffa
christopher laird <fclaird@yahoo.com>
2017-07-30 19:42:46 +0900

    leap js 2 lines

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,18 +1,10 @@
-//
-// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Year = function(input) {
-//
-// YOUR CODE GOES HERE
-//  
+    this.value = input;
 };
 
 Year.prototype.isLeap = function() {
-//
-// YOUR CODE GOES HERE
-//
+    if (this.value % 400 == 0 || (this.value % 4 == 0 && this.value % 100 != 0)) return true;
+    else return false;
 };
 
 module.exports = Year;
